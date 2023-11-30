a07a4e9fe31f1346d8d330e4d20f62098d11d048
Vivian Wu <vivian.stwu@gmail.com>
2023-11-30 16:23:43 -0300

    Interacao JS e correção de bugs

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const infosJoji = document.getElementById("infos-Joji");
+const infosLevi = document.getElementById("infos-Levi");
+const infosVivian = document.getElementById("infos-Vivian");
+
+
+function toggleJoji() {
+    if (infosJoji.style.display === "none") {
+      infosJoji.style.display = "block";
+    } else {
+      infosJoji.style.display = "none";
+    }
+  }
+
+  function toggleLevi() {
+    if (infosLevi.style.display === "none") {
+      infosLevi.style.display = "block";
+    } else {
+      infosLevi.style.display = "none";
+    }
+  }
+
+  function toggleVivian() {
+    if (infosVivian.style.display === "none") {
+      infosVivian.style.display = "block";
+    } else {
+      infosVivian.style.display = "none";
+    }
+  }
